cd3c82d2a111db36fa8d07d9c47b93fe74a6fef8
Sebastian <sebastiantorkowski@gmail.com>
2024-01-06 07:38:52 +0100

    custom map marker

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import markerIcon from "../assets/marker.png";
 
 const position = [51.505, -0.19];
 
@@ -13,10 +14,19 @@ function Map() {
       scrollWheelZoom={false}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
+
+      <Marker
+        position={position}
+        icon={L.icon({
+          iconUrl: markerIcon,
+          iconSize: [32, 32],
+          iconAnchor: [16, 32],
+          popupAnchor: [0, -32],
+        })}
+      >
         <Popup>London, Palace Green</Popup>
       </Marker>
     </MapContainer>
