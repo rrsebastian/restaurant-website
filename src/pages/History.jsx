2520f12fdefd466ee34745a618c53459e7e058c8
Sebastian <sebastiantorkowski@gmail.com>
2024-01-06 07:48:55 +0100

    test

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import Hero from "../Components/Hero";
 import backgroundImage from "../assets/bg-00011.jpg";
 import Navbar from "../Components/Navbar";
@@ -6,9 +6,24 @@ import TheFounder from "../Components/TheFounder";
 import Timeline from "../Components/Timeline";
 import BookATable from "../Components/BookATable";
 import Footer from "../Components/Footer";
+import { Redirect } from "react-router-dom";
 
 function History() {
+  const [shouldRedirect, setShouldRedirect] = useState(false);
   const bookATableRef = useRef();
+
+  useEffect(() => {
+    const isRefreshed =
+      window.performance.getEntriesByType("navigation")[0].type === "reload";
+
+    if (isRefreshed && window.location.pathname === "/history") {
+      setShouldRedirect(true);
+    }
+  }, []);
+
+  if (shouldRedirect) {
+    return <Redirect to="/history" />;
+  }
 
   return (
     <div>
